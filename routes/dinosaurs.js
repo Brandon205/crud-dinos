424b7b9e12ec1dd6a9537b5a840641845490a4df
Brandon205 <brandonblack02@gmail.com>
2019-10-08 14:24:38 -0700

    Can now add dinos

--- routes/dinosaurs.js
+++ routes/dinosaurs.js
@@ -8,6 +8,14 @@ router.get('/', function(req, res) {
     res.render('dinosaurs/index.ejs', {dinos: dinoData});
 });
 
+router.post('/', function(req, res) {
+    var dinos = fs.readFileSync('./dinosaurs.json');
+    var dinoData = JSON.parse(dinos);
+    dinoData.push(req.body);
+    fs.writeFileSync('./dinosaurs.json', JSON.stringify(dinoData));
+    res.redirect('/dinosaurs');
+})
+
 router.get('/new', function(req, res) {
     res.render('dinosaurs/new');
 });
@@ -18,6 +26,5 @@ router.get('/:id', function(req, res) {
     var dinoData = JSON.parse(dinos);
     res.render('dinosaurs/show', {dino: dinoData[index]});
 });
-
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,11 +6,12 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(ejsLayout);
 app.use(express.static('static')); // will show where the static or not rendered webpages will be 
+app.use(express.urlencoded({extended: false})); // Will take form submissions and it will put it into req.body
 
 app.get('/', function(req, res) {
     res.render('home.ejs');
 });
 
-app.use('/dinosaurs', require('./routes/dinosaurs'));
+app.use('/dinosaurs', require('./routes/dinosaurs.js'));
 
 app.listen(3000);
