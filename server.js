4083ce1928a3f760b982fc7021627fdc9806203a
Brandon205 <brandonblack02@gmail.com>
2019-10-08 11:22:00 -0700

    Server up and running

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const ejsLayout = require('express-ejs-layouts');
+const fs = require('fs');
+
+const app = express();
+app.set('view engine', 'ejs');
+app.use(ejsLayout);
+app.use(express.static('static')); // will show where the static or not rendered webpages will be 
+
+app.get('/', function(req, res) {
+    res.send('Welcome Home');
+});
+
+app.listen(3000);
