928f4ded7c464a76049a4d66a85265533e8c35f7
Brandon205 <brandonblack02@gmail.com>
2019-10-08 16:36:17 -0700

    Editing dinos works

--- routes/dinosaurs.js
+++ routes/dinosaurs.js
@@ -14,6 +14,22 @@ router.post('/', function(req, res) {
     dinoData.push(req.body);
     fs.writeFileSync('./dinosaurs.json', JSON.stringify(dinoData));
     res.redirect('/dinosaurs');
+});
+
+router.get('/edit/:id', function(req, res) {
+    var index = parseInt(req.params.id);
+    var dinos = fs.readFileSync('./dinosaurs.json');
+    var dinoData = JSON.parse(dinos);
+    res.render('dinosaurs/edit', {dino: dinoData[index], dinoIndex: index}); //needs a dino and a index
+});
+
+router.put('/:id', function(req, res) {
+    var index = parseInt(req.params.id);
+    var dinos = fs.readFileSync('./dinosaurs.json');
+    var dinoData = JSON.parse(dinos);
+    dinoData[index] = req.body;
+    fs.writeFileSync('./dinosaurs.json', JSON.stringify(dinoData));
+    res.redirect(`/dinosaurs/${index}`);
 })
 
 router.get('/new', function(req, res) {
@@ -24,7 +40,7 @@ router.get('/:id', function(req, res) {
     var index = parseInt(req.params.id);
     var dinos = fs.readFileSync('./dinosaurs.json');
     var dinoData = JSON.parse(dinos);
-    res.render('dinosaurs/show', {dino: dinoData[index]});
+    res.render('dinosaurs/show', {dino: dinoData[index], dinoIndex: index});
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const ejsLayout = require('express-ejs-layouts');
-const fs = require('fs');
+const methodOverride = require('method-override');
+
 
 const app = express();
 app.set('view engine', 'ejs');
 app.use(ejsLayout);
 app.use(express.static('static')); // will show where the static or not rendered webpages will be 
 app.use(express.urlencoded({extended: false})); // Will take form submissions and it will put it into req.body
+app.use(methodOverride('_method')); // Will look into the ACTION of the form and then it will do its thing will look for _method=put etc...
 
 app.get('/', function(req, res) {
     res.render('home.ejs');
