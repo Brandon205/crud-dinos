5bb927cc471f556a19f7ba6b0bcf7a61e0cbfe40
Brandon205 <brandonblack02@gmail.com>
2019-10-09 08:45:18 -0700

    Bit more styling

--- routes/cryptids.js
+++ routes/cryptids.js
@@ -2,13 +2,13 @@ const express = require('express');
 const fs = require('fs');
 const router = express.Router();
 
-router.get('/', function(req, res) {
+router.get('/', function(req, res) { // For the home page of the Cryptids
     var cryptids = fs.readFileSync('./cryptids.json');
     var cryptidData = JSON.parse(cryptids);
     res.render('cryptids/index.ejs', {cryptids: cryptidData});
 });
 
-router.post('/', function(req, res) {
+router.post('/', function(req, res) { // For the addition of a new cryptid
     var cryptids = fs.readFileSync('./cryptids.json');
     var cryptidData = JSON.parse(cryptids);
     cryptidData.push(req.body);
@@ -16,7 +16,7 @@ router.post('/', function(req, res) {
     res.redirect('/cryptids');
 });
 
-router.delete('/:id', function(req, res) {
+router.delete('/:id', function(req, res) { // For the deletion of a cryptid (from index.ejs)
     var index = parseInt(req.params.id);
     var cryptids = fs.readFileSync('./cryptids.json');
     var cryptidData = JSON.parse(cryptids);
@@ -25,14 +25,14 @@ router.delete('/:id', function(req, res) {
     res.redirect('/cryptids');
 });
 
-router.get('/edit/:id', function(req, res) {
+router.get('/edit/:id', function(req, res) { // For displaying the cryptid edit page 
     var index = parseInt(req.params.id);
     var cryptids = fs.readFileSync('./cryptids.json');
     var cryptidData = JSON.parse(cryptids);
     res.render('cryptids/edit', {cryptid: cryptidData[index], cryptidIndex: index});
 });
 
-router.put('/:id', function(req, res) {
+router.put('/:id', function(req, res) { // For the updating of the cryptids data
     var index = parseInt(req.params.id);
     var cryptids = fs.readFileSync('./cryptids.json');
     var cryptidData = JSON.parse(cryptids);
@@ -41,11 +41,11 @@ router.put('/:id', function(req, res) {
     res.redirect(`/cryptids/${index}`);
 });
 
-router.get('/new', function(req, res) {
+router.get('/new', function(req, res) { // For displaying the add new cryptid page 
     res.render('cryptids/new.ejs');
 });
 
-router.get('/:id', function(req, res) {
+router.get('/:id', function(req, res) { // For showing all of the cryptids
     var index = parseInt(req.params.id);
     var cryptids = fs.readFileSync('./cryptids.json');
     var cryptidData = JSON.parse(cryptids);
@@ -54,4 +54,4 @@ router.get('/:id', function(req, res) {
 
 
 
-module.exports = router;
+module.exports = router; // So that we can import it to the server 
--- server.js
+++ server.js
@@ -5,16 +5,17 @@ const methodOverride = require('method-override');
 
 const app = express();
 app.set('view engine', 'ejs');
-app.use(ejsLayout);
-app.use(express.static('static')); // will show where the static or not rendered webpages will be 
+
+app.use(ejsLayout); // Will allow us to use layouts, it will search in the views directory for something named layout.ejs
+app.use(express.static('static')); // Will show where the static or not rendered things will be, THIS is a great place for CSS and JS files
 app.use(express.urlencoded({extended: false})); // Will take form submissions and it will put it into req.body
 app.use(methodOverride('_method')); // Will look into the ACTION of the form and then it will do its thing will look for _method=put etc...
 
-app.get('/', function(req, res) {
-    res.render('home.ejs');
+app.get('/', function(req, res) { // This is a get request for the home directory
+    res.render('home.ejs'); //Render is a function that takes in 2 args, 1 is the ejs file to render, and the second is for {} with any data to pass in
 });
 
-app.use('/dinosaurs', require('./routes/dinosaurs.js'));
+app.use('/dinosaurs', require('./routes/dinosaurs.js')); // These are both how to use a ROUTES directory with different .js files inside
 app.use('/cryptids', require('./routes/cryptids.js'));
 
 app.listen(3000);
